2f65ed3e3b31d912981c6a5ab8db962cee6d6451
Erick Garcia Godoy <erickgarciagodoy@outlook.com>
2020-12-16 21:53:18 -0300

    Estruturas condicionais

--- Cap. 02/condicionais_start.py
+++ Cap. 02/condicionais_start.py
@@ -1,3 +1,15 @@
 #
 # Arquivo de exemplo das estruturas condicionais
 #
+def Condicionais():
+  x, y = 100, 100
+
+  if(x < y):
+    print("X é menor que Y")
+  elif (x == y):
+    print("X é igual a Y")
+  else:
+    print("X é maior que Y")
+
+# Sempre chamar a função, para funcionar
+Condicionais()
