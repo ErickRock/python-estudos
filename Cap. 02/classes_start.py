009506794e5e6b551486708ab5ae61f6c9d9a110
Erick Garcia Godoy <erickgarciagodoy@outlook.com>
2020-12-22 00:56:18 -0300

    Classes, Objetos, Construtor e funções

--- Cap. 02/classes_start.py
+++ Cap. 02/classes_start.py
@@ -1,3 +1,26 @@
 #
 # Exemplo de como criar classes
 #
+# __init__ é um construtor e todos os métodos recebem self como paramêtro, 
+# mas não precisa ser usando
+
+class minhaClasse():
+  def __init__(self):
+      self.meuAtributo = "Passou pelo meu construtor!"
+
+  def meuMetodo(self):
+      print("Passou pelo meu método")
+
+  def meuMetodo2(self, valor):
+      self.outroAtributo = valor
+      print(self.outroAtributo)
+
+def criarObjeto():
+  meuObj = minhaClasse()
+  var1 = meuObj.meuAtributo
+  print(var1)
+  
+  meuObj.meuMetodo()
+  meuObj.meuMetodo2("Executando um método")
+
+criarObjeto()
