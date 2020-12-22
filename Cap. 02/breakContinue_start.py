547ba7c9e75de91cfea65c6716891b56e639d295
Erick Garcia Godoy <erickgarciagodoy@outlook.com>
2020-12-22 00:31:44 -0300

    Break e Continue no loop

--- Cap. 02/breakContinue_start.py
+++ Cap. 02/breakContinue_start.py
@@ -1,3 +1,19 @@
 #
 # Exemplo de como usar os comando Break e Continue
-#
+# Break interrompe a execução do Loop
+# Continue interrompe UMA execução e passa para o próximo item
+def loopBreak():
+  for x in range(5,10):
+    if(x == 7):
+      break
+    print("O valor de X é: ", x)
+
+#loopBreak()
+
+def loopContinue():
+  for x in range(5,10):
+    if(x == 7):
+      continue
+    print("O valor de X é: ", x)
+
+loopContinue()
