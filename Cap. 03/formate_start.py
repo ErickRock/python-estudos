c11b36c7f5e6f88f188663082fe1541151bad699
Erick Garcia Godoy <erickgarciagodoy@outlook.com>
2020-12-25 22:10:12 -0300

    ✨ Formatando horas e datas

--- Cap. 03/formate_start.py
+++ Cap. 03/formate_start.py
@@ -6,14 +6,19 @@
 from datetime import datetime
 
 # Datas e horas podem ser formatados usando um conjunto de strings predefinidas
+def FormataDataHora():
   
 #### Date Formatting ####
   
 # %y/%Y - Ano, %a/%A - Dia da semana, %b/%B - Mês, %d - dia do mÊs
+  hoje = datetime.now()
 
+  print(hoje.strftime("O ano é: %Y"))
 
 #% c - data e hora da localidade,% x - data da localidade,% X - hora da localidade
-
+  print(hoje.strftime("Data e hora local é: %c"))
   
 #### Time Formatting ####
 # %I/%H - 12/24 hpras, %M - minuto, %S - segundo, %p -  AM/PM
+  print(hoje.strftime("A hora atual é: %I:%M:%S %p"))
+FormataDataHora()
