7d29b427fa31880d4b23d41417d9a87e07dcfba9
Erick Garcia Godoy <erickgarciagodoy@outlook.com>
2021-01-06 22:08:37 -0300

    :sparkles: Trabalhando com calendários

--- Cap. 03/exemploDeltas_start.py
+++ Cap. 03/exemploDeltas_start.py
@@ -7,3 +7,14 @@ from datetime import time
 from datetime import datetime
 from datetime import timedelta
 
+def QuantosDiasFaltam(ano, mes, dia):
+  hoje = date.today()
+  dataProcurada = date(ano, mes, dia)
+
+  qntsDias = dataProcurada - hoje
+
+  msgRetorno = "Faltam " + str(qntsDias).replace("days, 0:00:00", "") + " dias para a data " + dataProcurada.strftime("%d/%m/%y")
+
+  print(msgRetorno)
+
+QuantosDiasFaltam(2022, 1, 6)
